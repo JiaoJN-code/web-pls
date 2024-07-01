90ef6eacb24738df32a9f54ccf8106c8e0141a19
焦久宁 <1286704109@qq.com>
2024-07-01 11:48:55 +0800

    移除 Arco Design pro of vue 国际化

---
+++ src/router/routes/externalModules/arco.ts
@@ -0,0 +1,10 @@
+export default {
+  path: 'https://arco.design',
+  name: 'arcoWebsite',
+  meta: {
+    locale: 'Arco Design',
+    icon: 'icon-link',
+    requiresAuth: true,
+    order: 8,
+  },
+};
---
+++ src/router/routes/externalModules/faq.ts
@@ -0,0 +1,10 @@
+export default {
+  path: 'https://arco.design/vue/docs/pro/faq',
+  name: 'faq',
+  meta: {
+    locale: '常见问题',
+    icon: 'icon-question-circle',
+    requiresAuth: true,
+    order: 9,
+  },
+};
